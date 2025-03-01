5fc8c035b03bc99d89a2ffee33a05589587c951b
Jose Luis <wildchamo@gmail.com>
2025-03-01 13:21:39 -0500

    initialize React app with Vite setup and basic component structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { ModulesExample } from "./examples/modules";
+function App() {
+  return (
+    <>
+      <ModulesExample></ModulesExample>
+    </>
+  );
+}
+
+export default App;
---
+++ src/examples/modules/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export const ModulesExample = () => {
+  return (
+    <section className={styles.container}>
+      <p className={styles.important_P}>Im a Component using css modules!</p>
+    </section>
+  );
+};
