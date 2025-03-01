3feb0c805c4177287baad1846562a6b0efad6e4d
Jose Luis <wildchamo@gmail.com>
2025-03-01 13:30:23 -0500

    add Tailwind CSS integration and example component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import { ModulesExample } from "./examples/modules";
 import { StyledComponent } from "./examples/styled-components";
+import { TailwindExample } from "./examples/tailwind";
+
 function App() {
   return (
     <>
+      <TailwindExample />
       <StyledComponent color="pink" />
     </>
   );
---
+++ src/examples/tailwind/index.jsx
@@ -0,0 +1,3 @@
+export const TailwindExample = () => {
+  return <p className="text-blue-600">hola</p>;
+};
